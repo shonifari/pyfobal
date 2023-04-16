75dc92514faee8538fdd0e5aeeda207f6abe4348
shonifari <karis.omotosho@gmail.com>
2023-04-16 17:29:47 +0100

    Fixed directory-tree

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="pyfobal",
@@ -11,4 +16,6 @@ setup(
     author_email="karomo.sm@gmail.com",
     description="A Python SDK for the Fobal API",
     url="https://github.com/shonifari/pyfobal",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
